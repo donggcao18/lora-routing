2d22d9ba5fcac9f17bad7d34df24437542c2baa8
donggcao18 <caovdongg18@gmail.com>
2025-10-07 20:50:46 +0700

    +

--- CoIN/llava/train/CoIN/peft/tuners/lora.py
+++ CoIN/llava/train/CoIN/peft/tuners/lora.py
@@ -185,6 +185,7 @@ class LoraModel(torch.nn.Module):
         self.add_adapter(adapter_name, self.peft_config[adapter_name])
         self.ema_modules = {}
         self.p_grad_data = {}
+        self.p_data = {}
 
         # transformers models have a .config attribute, whose presence is assumed later on
         if not hasattr(self, "config"):
@@ -632,6 +633,27 @@ class LoraModel(torch.nn.Module):
         """
         return self._unload_and_optionally_merge(merge=False)
 
+    # def ema_update(self, model):
+    #     for n, p in model.named_parameters():
+    #         if p.requires_grad and self.dema:
+    #             if n not in self.ema_modules:
+    #                 self.ema_modules[n] = copy.deepcopy(p)
+    #             with torch.no_grad():
+    #                 try:
+    #                     a = torch.norm(p.data - self.ema_modules[n].data, p=1)  # theta_t - theta_{t-1}^*
+    #                     b = torch.norm(p.grad.data, p=1)  # L'
+    #                     c = torch.norm(p.grad.data - self.p_grad_data[n].data, p=1)  # L''
+                        
+    #                     alpha = float((1 + b) / (a * c) * 2)
+    #                     if 0 < alpha < 1:
+    #                         alpha = alpha
+    #                     else:
+    #                         alpha = 0.01
+    #                     self.ema_modules[n].data = self.ema_modules[n].data * (1 - alpha) + p.data * alpha
+    #                 except Exception :
+    #                     self.ema_modules[n].data = self.ema_modules[n].data * 0.99 + p.data * 0.01
+    #             self.p_grad_data[n] = copy.deepcopy(p.grad)
+
     def ema_update(self, model):
         print(f"dema {self.dema}")
         for n, p in model.named_parameters():
@@ -643,8 +665,10 @@ class LoraModel(torch.nn.Module):
                         a = torch.norm(p.data - self.ema_modules[n].data, p=1)  # theta_t - theta_{t-1}^*
                         b = torch.norm(p.grad.data, p=1)  # L'
                         c = torch.norm(p.grad.data - self.p_grad_data[n].data, p=1)  # L''
+                        d = torch.norm(p.data - self.p_data[n].data, p=1)
+                        
                         print("a, b, c:", a, b, c)
-                        alpha = float((1 + b) / (a * c) * 2)
+                        alpha = float((1 + b) * d/ (a * c))
                         print("alpha:", alpha)
                         if 0 < alpha < 1:
                             alpha = alpha
@@ -656,6 +680,7 @@ class LoraModel(torch.nn.Module):
                         self.ema_modules[n].data = self.ema_modules[n].data * 0.99 + p.data * 0.01
                         print("EMA update exception in {n} with exeption {e}".format(n=n, e=e))
                 self.p_grad_data[n] = copy.deepcopy(p.grad)
+                self.p_data[n] = copy.deepcopy(p)
 
     def ema_replace(self, model):
         for n, p in model.named_parameters():
